c5fed885a790e0c2e905f684b2297aeca4e530cb
dandancell <dandancell.recife@gmail.com>
2025-09-21 14:46:58 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ export const TIMEZONE = "America/Recife"; // Recife (sem horário de verão)
 export const BUSINESS_HOURS = {
   0: ["10:00-14:30"], // Domingo
   1: [], 2: [], 3: [], 4: [], 5: [],
-  6: ["10:00-15:50"]  // Sábado
+  6: ["10:00-15:30"]  // Sábado
 };
 
 // Fechado: bloquear pedido e mostrar banner
