782dd3a47070ffc5fa9bfb800e624a85e1811db6
dandancell <dandancell.recife@gmail.com>
2025-09-27 14:01:38 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ export const ESGOTADOS = [
   "COCA-COLA PET 2 L", 
   "MACARRÃO NA PRESSÃO",            // deixe aqui só se quer mesmo bloquear FEIJOADA
   "PUDIM DE LEITE",
-  ""
+  "LASANHA"
 ];
 
 // Horário de funcionamento: APENAS sábados e domingos, 11:00–15:00
