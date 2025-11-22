d02a4e442bef8d845d6a21f53e7abc5f62468a91
dandancell <dandancell.recife@gmail.com>
2025-11-22 10:22:51 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ export const BAIRROS = [
 // Controle de estoque simples (nomes EXACTOS em MAIÚSCULAS, como no cardápio)
 export const ESGOTADOS = [
   "COCA-COLA PET 2 L", 
-  "ESCONDIDINHO DE CHARQUE",            // deixe aqui só se quer mesmo bloquear FEIJOADA
+  "",            // deixe aqui só se quer mesmo bloquear FEIJOADA
   "PUDIM DE LEITE",
   "",
   "",
